e40f9ed0ede8cf1dab4843a58ce17049bb41b8ef
J-Etienne1 <58144689+J-Etienne1@users.noreply.github.com>
2022-07-13 18:40:48 +0100

    Update settings.py
    removed secret key

--- djangoFinalYrProjectPrep/settings.py
+++ djangoFinalYrProjectPrep/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lx9*#y5qmv=l&lef)a(c#dbgncikttzmd8)%%krixb-vv)z2@)'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
